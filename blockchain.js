0cc9eabab1737bb51ee7419f7acb8b9c96d0928a
harmeet27 <harmeetkaur2793@gmail.com>
2018-12-25 21:35:41 +0530

    feat : isValidChain and Replacement

--- blockchain.js
+++ blockchain.js
@@ -1,8 +1,7 @@
+const crypto = require("./crypto-hash");
 const Block = require("./block");
 class Blockchain {
   constructor() {
-    console.log("hi");
-    console.log(Block.genisis());
     this.blockchainArray = [Block.genisis()];
   }
 
@@ -12,6 +11,38 @@ class Blockchain {
       data
     );
     this.blockchainArray.push(newBlock);
+  }
+
+  validateChain(blockchainArray) {
+    if (
+      JSON.stringify(blockchainArray[0]) !== JSON.stringify(Block.genisis())
+    ) {
+      return false;
+    }
+
+    for (let i = 1; i < blockchainArray.length; i++) {
+      const block = blockchainArray[i];
+      const { timeStamp, lastHash, hash, data } = block;
+      const prevBlock = blockchainArray[i - 1];
+      if (hash !== crypto(timeStamp, lastHash, data)) {
+        return false;
+      }
+      if (prevBlock.hash !== lastHash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  replaceChain(newChain) {
+    if (newChain.length <= this.blockchainArray.length) {
+      return;
+    }
+    if (!this.validateChain(newChain)) {
+      return;
+    }
+    this.blockchainArray = chain;
   }
 }
 
--- blockchain.test.js
+++ blockchain.test.js
@@ -2,7 +2,14 @@ const Blockchain = require("./blockchain");
 const Block = require("./block");
 
 describe("blockchain class functionality test", () => {
-  const blockChain = new Blockchain();
+  let blockChain;
+  let newChain;
+  let originalChain;
+  beforeEach(() => {
+    blockChain = new Blockchain();
+    newChain = new Blockchain();
+    originalChain = blockChain.blockchainArray;
+  });
   it("blockchain array check", () => {
     expect(blockChain.blockchainArray instanceof Array).toBe(true);
   });
@@ -18,5 +25,71 @@ describe("blockchain class functionality test", () => {
     expect(
       blockChain.blockchainArray[blockChain.blockchainArray.length - 1].data
     ).toEqual(data);
+  });
+
+  describe("valid blockchain test", () => {
+    beforeEach(() => {
+      blockChain.addBlock({ data: "block1" });
+      blockChain.addBlock({ data: "block2" });
+      blockChain.addBlock({ data: "block3" });
+    });
+
+    describe("first block check for genesis", () => {
+      it("return false", () => {
+        blockChain.blockchainArray[0].data = ["fake data"];
+        expect(blockChain.validateChain(blockChain.blockchainArray)).toBe(
+          false
+        );
+      });
+
+      it("return false in case of chain invalid", () => {
+        blockChain.blockchainArray[2].data = "wrong-value";
+        expect(blockChain.validateChain(blockChain.blockchainArray)).toBe(
+          false
+        );
+      });
+
+      it("return false in case of last hash is different", () => {
+        blockChain.blockchainArray[2].lastHash = "fdgshj";
+        expect(blockChain.validateChain(blockChain.blockchainArray)).toBe(
+          false
+        );
+      });
+      it("return true for correct chain", () => {
+        expect(blockChain.validateChain(blockChain.blockchainArray)).toBe(true);
+      });
+
+      it("return true for correct chain1", () => {
+        blockChain.blockchainArray[0].hash = "fdgshj";
+        expect(blockChain.validateChain(blockChain.blockchainArray)).toBe(
+          false
+        );
+      });
+    });
+  });
+
+  describe("replaceChian()", () => {
+    beforeEach(() => {
+      newChain.addBlock({ data: "block1" });
+      newChain.addBlock({ data: "block2" });
+      newChain.addBlock({ data: "block3" });
+    });
+    describe("when chain is not longer", () => {
+      it("does not replace the chian", () => {
+        newChain.blockchainArray[0] = { new: "data" };
+        blockChain.replaceChain(newChain.blockchainArray);
+        expect(blockChain.blockchainArray).toEqual(originalChain);
+      });
+    });
+
+    describe("when chain is longer", () => {
+      describe("when chain is not valid", () => {
+        it("does not replace the chian", () => {
+          newChain.blockchainArray[2].lastHash = "ggygs";
+          blockChain.replaceChain(newChain.blockchainArray);
+          expect(blockChain.blockchainArray).toEqual(originalChain);
+        });
+      });
+    });
   });
 });
