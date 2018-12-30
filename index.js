d374de513caa354c78fc0f2e5d0bfcbee7c40c80
harmeet27 <harmeetkaur2793@gmail.com>
2018-12-30 17:07:42 +0530

    feat:POST addBlock added and PubSub initial prototype

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const BlockChain = require("./blockchain");
+const PubSub = require("./pubsub");
 
 const app = express();
+app.use(bodyParser.json());
+
 const blockchain = new BlockChain();
+
+const pubsub = new PubSub({ blockchain });
+
+setTimeout(() => pubsub.broadCastChain(), 2000);
 
 app.get("/api/blocks", (req, res) => {
   res.json(blockchain.blockchainArray);
+});
+
+app.post("/api/addBlock", (req, res) => {
+  const { data } = req.body;
+  blockchain.addBlock({ data });
+  res.redirect("/api/blocks");
 });
 
 const PORT = 3000;
---
+++ pubsub.js
@@ -0,0 +1,46 @@
+const redis = require("redis");
+const CHANNELS = {
+  TEST: "TEST",
+  BLOCKCHAIN: "Blockchain"
+};
+
+class PubSub {
+  constructor({ blockchain }) {
+    this.blockchain = blockchain;
+    this.publisher = redis.createClient();
+    this.subscriber = redis.createClient();
+
+    this.subscribeToChannels();
+
+    this.subscriber.on("message", (channel, message) =>
+      this.handleMessage(channel, message)
+    );
+  }
+
+  handleMessage(channel, message) {
+    const parsedMessage = JSON.parse(message);
+    if (channel === CHANNELS.BLOCKCHAIN) {
+      console.log(`${channel} is and message ${message}`);
+      this.blockchain.replaceChain(parsedMessage);
+    }
+  }
+
+  subscribeToChannels() {
+    Object.values(CHANNELS).forEach(channel =>
+      this.subscriber.subscribe(channel)
+    );
+  }
+
+  publish({ channel, message }) {
+    this.publisher.publish(channel, message);
+  }
+
+  broadCastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.blockchainArray)
+    });
+  }
+}
+
+module.exports = PubSub;
